c76326dd1e9a79a0407d196767ddd4ed19c41456
tangthiendat <thiendattang@gmail.com>
2024-07-04 10:45:58 +0700

    Centralize exception handling

---
+++ src/main/java/com/ttdat/identityservice/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.ttdat.identityservice.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+}
